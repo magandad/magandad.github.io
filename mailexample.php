38798e4c46af7d9eaed1d92017097e615c9ae8cb
Michael Brewer <brewer@uga.edu>
2024-09-05 14:02:59 -0400

    MB: Initial commit of few files

---
+++ mailexample.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<title>PLC</title>
+<?php 
+ $headers = "From: Vasu Renganathan<vasur@ccat.sas.upenn.edu>\n";
+
+// $headers .= "cc: mbrinkle@uga.edu, vasur@ccat.sas.upenn\n"; 
+// $headers .= "bcc: $bcc"; // BCCs to
+
+mail("vasur@ccat.sas.upenn.edu", "Test message", "This is the message", $headers);
+      
+	 	
+?>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+
+<body>
+Test succeeded.  Mail was sent.
+</body>
+</html>
